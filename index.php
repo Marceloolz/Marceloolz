536b187242142c96668b12174ea43867af9bef47
Marceloolz <marcelooferreira@outlook.com.br>
2021-06-06 16:24:18 -0300

    Ajustes antes de iniciar o projeto

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8" />
+    <title></title>
+    <!-- mobile responsive meta -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap"
+        rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name='description' content='' />
+    <meta name='keywords' content='' />
+    <meta property="og:image" content="">
+    <meta property="og:title" content="">
+    <meta property="og:description" content="">
+    <meta property="og:url" content="">
+    <meta property="og:type" content="article">
+    <meta property="og:locale" content="pt-br">
+    <meta property="og:site_name" content="">
+    <link rel="shortcut icon" href="" type="image/x-icon">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="">
+    <meta name="theme-color" content="#10D8B4">
+    <link rel="icon" type="image/png" href="" />
+    <!-- main template stylesheet -->
+    <link href="lib/fontawesome-free-5.9.0-web/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="dist/css/style.css?v=12">
+    <link rel="stylesheet" type="text/css" href="lib/slick/slick.css" />
+</head>
+
+<body>
+    <!-- <div class="preloader"></div> -->
+    
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
+    <script type="text/javascript" src="lib/slick/slick.min.js"></script>
+    <script src="js/app.js?v=1.0.0"></script>
+</body>
+
+</html>
