fc1aae8454253d123d0addf592b285a9cf6e2863
Marceloolz <marcelooferreira@outlook.com.br>
2021-06-13 15:58:07 -0300

    Codepen Pronto

--- js/app.js
+++ js/app.js
@@ -3,3 +3,13 @@ $(window).on('load', function() {
         $('.preloader').fadeOut('slow');
     }
   });
+
+  $('.Container_Galeria_Item').click(function(){
+    var ItemGaleriaPortfolio = $(this);
+    ItemGaleriaPortfolio.toggleClass( "__CodePenActive");
+  });
+
+  $('.Item_ModalCode_CloseBTN').click(function(){
+    $('.Container_Galeria_Item').removeClass( "__CodePenActive");
+    event.stopPropagation();
+  });
