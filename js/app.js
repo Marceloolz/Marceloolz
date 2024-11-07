e8ee4073cb599b6414d6e86b2ac4f0bc2b885375
Marcelo Gomes Ferreira <mgomes@marlin.com.br>
2024-11-07 10:43:28 -0300

    Ano e idade dinamicos

--- js/app.js
+++ js/app.js
@@ -1,19 +1,33 @@
+
 $(window).on('load', function() {
     if ($('.preloader').length) {
         $('.preloader').fadeOut('slow');
     }
-  });
+});
 
-  $('.Container_Galeria_Item').click(function(){
-    var ItemGaleriaPortfolio = $(this);
-    ItemGaleriaPortfolio.toggleClass( "__CodePenActive");
-  });
+$('.Container_Galeria_Item').click(function(){
+  var ItemGaleriaPortfolio = $(this);
+  ItemGaleriaPortfolio.toggleClass( "__CodePenActive");
+});
 
-  $('.Item_ModalCode_CloseBTN').click(function(){
-    $('.Container_Galeria_Item').removeClass( "__CodePenActive");
-    event.stopPropagation();
-  });
+$('.Item_ModalCode_CloseBTN').click(function(){
+  $('.Container_Galeria_Item').removeClass( "__CodePenActive");
+  event.stopPropagation();
+});
 
-  $('.SeletorDayNight').click(function(){
-    $('body').toggleClass( '__Is-Day');
-  });
+$('.SeletorDayNight').click(function(){
+  $('body').toggleClass( '__Is-Day');
+});
+
+document.getElementById("Footer_Container_AnoAtual").innerHTML = new Date().getFullYear();
+
+/* implementação da idade dinâmica */
+
+let anoNascimento = 1996;
+let anoAtual = new Date().getFullYear();
+
+let idade = anoAtual - anoNascimento;
+
+document.getElementById("MinhaIdade").innerHTML = idade;
+
+/* Fim da idade dinâmica */
